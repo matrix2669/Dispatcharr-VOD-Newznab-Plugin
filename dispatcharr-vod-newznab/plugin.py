46ec44fd52bc2f7553a618f6e2f4e15a4aad49b8
matrix2669 <jarred@jdscomputing.com>
2026-08-12 18:00:18 -0400

    Set service version to 0.1.9

--- dispatcharr-vod-newznab/plugin.py
+++ dispatcharr-vod-newznab/plugin.py
@@ -34,7 +34,7 @@ log = _PluginLogAdapter(logging.getLogger("apps.plugins.loader"), {})
 
 class Plugin:
     name = PLUGIN_NAME
-    version = "0.1.8"
+    version = "0.1.9"
     description = "Newznab + SABnzbd bridge for raw Dispatcharr VOD providers backed by Mustarrd."
     author = "matrix2669"
 
