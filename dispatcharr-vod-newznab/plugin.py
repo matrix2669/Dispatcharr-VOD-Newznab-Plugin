1e755dd4f60ee543dc8c119cc342ada2dd356d75
matrix2669 <jarred@jdscomputing.com>
2026-08-12 17:01:47 -0400

    Bump plugin version to 0.1.6

--- dispatcharr-vod-newznab/plugin.py
+++ dispatcharr-vod-newznab/plugin.py
@@ -32,7 +32,7 @@ log = _PluginLogAdapter(logging.getLogger("apps.plugins.loader"), {})
 
 class Plugin:
     name = PLUGIN_NAME
-    version = "0.1.5"
+    version = "0.1.6"
     description = "Newznab + SABnzbd bridge for raw Dispatcharr VOD providers backed by Mustarrd."
     author = "matrix2669"
 
